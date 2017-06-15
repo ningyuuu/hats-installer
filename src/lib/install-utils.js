60a8dd7387dc5fc919f0021473eb84db19b0e776
Neurrone <dickson@u.nus.edu>
2017-06-15 17:08:02 +0800

    Installation: autoapply registry patch for win7 so that testing works in IE

--- src/lib/install-utils.js
+++ src/lib/install-utils.js
@@ -1,9 +1,11 @@
 /**
  * Created by ningyu on 30/5/17.
  */
+const assert = require('assert');
+const childProcess = require('child_process');
 const log = require('electron-log');
+const os = require('os');
 const path = require('path');
-
 const Promise = require('promise');
 const request = require('request');
 const unzip = require('unzip');
@@ -16,6 +18,10 @@ const CMD_PIP_INSTALL = `pip install -r "${PIP_INSTALL_LIST_PATH}"`;
 const CMD_GET_HATS_DIR = 'echo %HATS%';
 const CMD_SET_HATS_DIR = `setx HATS "${WIN_INSTALL_LOCATION}" /M`;
 const CMD_SET_SYS_ENV_PATH = 'setx PATH "%path%;%HATS%" /M';
+const WIN7_X64_REG_PATCH = 'REG ADD "HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Microsoft\\Internet Explorer\\MAIN'
+  + '\\FeatureControl\\FEATURE_BFCACHE" /v iexplore.exe /t REG_DWORD /d 0 /f';
+const WIN7_X86_REG_PATCH = 'REG ADD "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Internet Explorer\\MAIN'
+  + '\\FeatureControl\\FEATURE_BFCACHE" /v iexplore.exe /t REG_DWORD /d 0 /f';
 
 const drivers = [
   'https://chromedriver.storage.googleapis.com/2.29/chromedriver_win32.zip',
@@ -27,8 +33,8 @@ const utils = {};
 
 utils.unzipUrlAsStream = url => (
     request({
-      followAllRedirects: true, 
-      url: url
+      followAllRedirects: true,
+      url
     })
       .pipe(unzip.Extract({
         path: WIN_INSTALL_LOCATION
@@ -63,9 +69,47 @@ utils.extractBrowserDrivers = () => {
   }
 };
 
+const isWin7 = () => os.platform() === 'win32' && /^6\.1/.test(os.release());
+
+// returns 32 or 64, depending on the OS's architecture
+const getBitness = () => {
+  const output = childProcess.execSync('wmic OS get OSArchitecture | findstr bit').toString();
+  const matches = /(\d+)-bit/.exec(output);
+  assert.ok(matches.length >= 2, 'Could not determine OS architecture, wmic unexpected output');
+  const bitness = parseInt(matches[1], 10);
+  assert.ok(bitness === 32 || bitness === 64, 'Architecture is neither 32 or 64 bit');
+  return bitness;
+};
+
+// Applies a registry patch so that tests run on Win7 IE
+const applyIePatchIfRequired = () => new Promise((resolve, reject) => {
+  if (isWin7()) {
+    let cmd = '';
+    switch (getBitness()) {
+      case 32:
+        cmd = WIN7_X86_REG_PATCH;
+        break;
+      case 64:
+        cmd = WIN7_X64_REG_PATCH;
+        break;
+      default:
+        reject('Unrecognized architecture');
+    }
+
+    common.execAsyncCommand(cmd)
+    .catch((err) => {
+      reject(`Could not apply Win7 IE patch: ${err}`);
+    });
+  } else {
+    resolve();
+  }
+});
+
 utils.installOnWindows = () => new Promise((resolve, reject) => {
   // install all prerequisites and requirements
-  common.execAsyncCommand(CMD_PIP_INSTALL).then(() => {
+  common.execAsyncCommand(CMD_PIP_INSTALL)
+  .then(applyIePatchIfRequired)
+  .then(() => {
     utils.extractBrowserDrivers();
     resolve();
   }).catch((err) => {
