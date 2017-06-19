3196b74f533953eddb67be0e60366f35ae6b395b
Ning Yu <ningyu321@gmail.com>
2017-06-19 10:53:10 +0800

    check __dirname

--- start.js
+++ start.js
@@ -4,4 +4,6 @@
 const wincmd = require('node-windows');
 const path = require('path');
 
+console.log(path.resolve(__dirname, 'node_modules', 'electron'));
+
 wincmd.elevate('electron '+__dirname, {cwd: path.resolve(__dirname, 'node_modules', 'electron')});
