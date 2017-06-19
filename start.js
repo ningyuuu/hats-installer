2bc0bee0325a6ea0f4bfef97cff60eb6b18ccda7
Ning Yu <ningyu321@gmail.com>
2017-06-18 20:15:04 -0700

    change to node_modules/.bin

--- start.js
+++ start.js
@@ -8,4 +8,4 @@ console.log(path.resolve(__dirname, 'node_modules', 'electron'));
 const dirname = path.resolve(__dirname);
 console.log(__dirname);
 
-wincmd.elevate('electron '+dirname, {cwd: path.resolve(__dirname, 'node_modules', 'electron')});
+wincmd.elevate('electron '+dirname, {cwd: path.resolve(__dirname, 'node_modules', '.bin')});
