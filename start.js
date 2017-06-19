a6b9e416cf600e791caf962cfdabcdf30531151f
Ning Yu <ningyu321@gmail.com>
2017-06-19 11:53:14 +0800

    Final commit for working start.js

--- start.js
+++ start.js
@@ -5,6 +5,6 @@ const wincmd = require('node-windows');
 const path = require('path');
 
 const dirname = path.resolve(__dirname);
-console.log(__dirname);
+console.log('About to initialize electron app at ' + dirname);
 
 wincmd.elevate('electron ' + dirname, {cwd: path.resolve(__dirname, 'node_modules', '.bin')});
